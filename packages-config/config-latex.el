0942586b3e94b6a3657f9827313a451ec17dbdf0
Adrien Suau <adrien.suau@cerfacs.fr>
2019-08-20 10:29:24 +0200

    Improved AucTeX loading

--- packages-config/config-latex.el
+++ packages-config/config-latex.el
@@ -1,12 +1,19 @@
-(use-package tex
+(use-package tex-site
   :straight auctex
+  :init
   :config
-  ;; Turn on RefTeX in AUCTeX
-  (add-hook 'TeX-mode-hook 'turn-on-reftex)
   ;; Activate nice interface between RefTeX and AUCTeX
   (setq reftex-plug-into-AUCTeX t)
+  ;; Load RefTeX
+  (require 'reftex)
+  ;; From https://tex.stackexchange.com/a/278060/166093
+  (setq reftex-label-alist '(AMSTeX))
+  ;; From https://tex.stackexchange.com/questions/139824/disabling-the-select-reference-format-menu-in-reftex
+  (setq reftex-ref-macro-prompt nil)
+  ;; Turn on RefTeX in AUCTeX
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
   ;; Turn on folding
   ;; From https://www.gnu.org/software/auctex/manual/auctex/Folding.html
-  (add-hook 'TeX-mode-hook (lambda ()
-                             (TeX-fold-mode 1)))
+  (add-hook 'LaTeX-mode-hook (lambda ()
+                               (TeX-fold-mode 1)))
   )
