43bbd9baa198b6321841b70713c25c0a21f394ba
Adrien Suau <adrien.suau@cerfacs.fr>
2019-09-06 11:03:53 +0200

    Add smartparens for Python

--- packages-config/config-smartparens.el
+++ packages-config/config-smartparens.el
@@ -1,4 +1,5 @@
 (use-package smartparens
   :config
   (require 'smartparens-config)
-  (add-hook 'TeX-mode-hook #'smartparens-mode))
+  (add-hook 'TeX-mode-hook #'smartparens-mode)
+  (add-hook 'python-mode-hook #'smartparens-mode))
